cbabf301c6f79d5d358764adba13c60396f73002
Hannah Osei <hannah.osei@softwire.com>
2025-04-23 16:21:31 +0100

    completed task - counts all domains using dictionary

--- emailExtraction.js
+++ emailExtraction.js
@@ -1,32 +1,28 @@
 const fs = require('fs')
 const fileContents = fs.readFileSync('Emails.txt').toString();
 
-const softwireEmailRegex = /@softwire\.com\s/g;
 const allEmailRegex =/\S+@\S+\.\S+/g;
-
-const softwireMatches = fileContents.match(softwireEmailRegex)
 const allEmailMatches = fileContents.match(allEmailRegex);
-
 
 let groupedByDomain = {};
 
 function readAndParseSoftWireEmails() {
     let counter = 0;
    
-    for (let i=0; i<allEmailMatches.length; i++) {
+    for (let i = 0; i < allEmailMatches.length; i++) {
         let firstPart = allEmailMatches[i];        
-        let secondPart = firstPart.split('@')[1];       
+        let secondPart = firstPart.split('@')[1];
 
-        for (let j =i+1; j<allEmailMatches.length; j++) {
-            if(allEmailMatches[j].includes(secondPart)) {
-                counter ++;
-                groupedByDomain[secondPart] = counter;
-            }
-            
+        if (groupedByDomain[secondPart]) {
+            groupedByDomain[secondPart] += 1;
+        } else {
+            groupedByDomain[secondPart] = 1;
         }
-        counter=0;
     }
-    console.log(groupedByDomain);
+
+   for (let dictionaryKey in groupedByDomain) {
+    console.log(`${dictionaryKey}: ${groupedByDomain[dictionaryKey]}`);
+   }
 }
 
 readAndParseSoftWireEmails();
