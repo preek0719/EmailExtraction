daddc0f51d07c9b89c8e1616a98a2839d662b20c
Hannah Osei <hannah.osei@softwire.com>
2025-04-23 13:43:55 +0100

    Used Regex to match softwire emails

--- emailExtraction.js
+++ emailExtraction.js
@@ -1,23 +1,23 @@
 
 const { open } = require('node:fs/promises');
-
- function readAndParseSoftWireEmails() { 
-    let counter = 0;    
+const emailRegex = /(.+)@softwire\.com$/;
+function readAndParseSoftWireEmails() {
+    let counter = 0;
     (async () => {
-        const file = await open('Emails.txt');       
+        const file = await open('Emails.txt');
         for await (const line of file.readLines()) {
-            let words = line.split(' ');         
-            for(let i = 0; i < words.length; i++) {
-                if ((words[i].substring(words[i].length -13) == '@softwire.com')) {
-                    counter ++;
+            let words = line.split(' ');
+            for (let i = 0; i < words.length; i++) {
+                if ((words[i].match(emailRegex))) {
+                    counter++;
                 }
-            } 
-        }         
-        console.log('The number of emails with @softwire.com domain is '+ counter); 
+            }
+        }
+        console.log('The number of emails with @softwire.com domain is ' + counter);
     }
     )
-    ();
+        ();
 }
 
 readAndParseSoftWireEmails();
-  
+
